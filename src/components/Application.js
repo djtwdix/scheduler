23ad2415727898f71d011f1f517cc3daada81c53
djtwdix <djtwd.ix@icloud.com>
2021-03-18 08:04:48 -0300

    Add interviewers to state and assign api data

--- src/components/Application.js
+++ src/components/Application.js
@@ -10,7 +10,8 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   })
   
   const setDay = day => setState({...state, day})
@@ -23,12 +24,11 @@ export default function Application(props) {
       axios.get("/api/appointments"),
       axios.get("/api/interviewers")
     ]).then((all) => {
-      console.log("all 1:", all[1], "all 2: ", all[2])
-      setState(prev => ({...prev, days:all[0].data, appointments:all[1].data}))
+      setState(prev => ({...prev, days:all[0].data, appointments:all[1].data, interviewers:all[2].data}))
     })
-
+    
   }, [])
-
+  
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
 
