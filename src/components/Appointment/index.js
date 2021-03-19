01088810f81728f921f6a8dc86d7b7dc9da4af5a
djtwdix <djtwd.ix@icloud.com>
2021-03-19 10:02:57 -0300

    Transition to show after booking interview

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -27,8 +27,8 @@ export default function Appointment(props) {
       student: name,
       interviewer
     }
-    
     props.bookInterview(props.id, interview)
+    transition(SHOW)
   }
 
   return(
