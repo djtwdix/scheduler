c79ff15377327f91e6cdead8ad29d812289812ac
djtwdix <djtwd.ix@icloud.com>
2021-03-21 12:20:42 -0300

    Refactor upDate spots function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -27,12 +27,7 @@ export const getInterviewersForDay = (state, day) => {
 
 export function updateSpots(days, appointments, id, value) {
   days.forEach(day => {
-    if (!appointments[id].interview && value === -1) {
-      if(day.appointments.includes(id)) {
-        day.spots = parseInt(day.spots) + value
-      }
-    }
-    if (value === 1) {
+    if ((!appointments[id].interview && value === -1) || value === 1) {
       if(day.appointments.includes(id)) {
         day.spots = parseInt(day.spots) + value
       }
