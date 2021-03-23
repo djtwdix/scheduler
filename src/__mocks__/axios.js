7d4867bebec395a11d3268039dd2e51fd2d448ba
djtwdix <djtwd.ix@icloud.com>
2021-03-23 17:42:31 -0300

    Add second put for edit

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -84,9 +84,15 @@ export default {
         statusText: "No Content",
       })
     }
+    if (url === "/api/appointments/2") {
+      return Promise.resolve({
+        status: 204,
+        statusText: "No Content"
+      })
+    }
   }),
   delete: jest.fn(url => {
-    if (url === "/api/appointments/1") {
+    if (url === "/api/appointments/2") {
       return Promise.resolve({
         status: 204,
         statusText: "No Content",
