e32084665db90a5730f3d7e6a5e6688073319492
djtwdix <djtwd.ix@icloud.com>
2021-03-24 16:04:14 -0300

    Write test for edit interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -2,13 +2,21 @@
 
 describe("Appointments", () => {
   beforeEach(() => {
-    cy.visit("/")
     cy.request("GET", "/api/debug/reset")
+    cy.visit("/")
     cy.contains("Monday")
   })
   it("should book an interview", () => {
-    cy.get(":nth-child(2) > .appointment__add > .appointment__add-button").click()
+    cy.get("[alt=Add]").first().click()
     cy.get("[data-testid=student-name-input]").type("Daniel James")
+    cy.get("[alt='Sylvia Palmer']").click()
+    cy.contains("Save").click()
+    cy.contains(".appointment__card--show", "Daniel James")
+    cy.contains(".appointment__card--show", "Sylvia Palmer")
+  })
+  it.only("should edit an interview", () => {
+    cy.get("[alt=Edit]").first().click({force:true})
+    cy.get("[data-testid=student-name-input]").clear().type("Daniel James")
     cy.get("[alt='Sylvia Palmer']").click()
     cy.contains("Save").click()
     cy.contains(".appointment__card--show", "Daniel James")
