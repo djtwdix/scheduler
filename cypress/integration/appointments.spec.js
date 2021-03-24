cb4a4438ce5a535061927092fc16ac5d26f9b61f
djtwdix <djtwd.ix@icloud.com>
2021-03-24 16:11:14 -0300

    Write test for deleting interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -14,13 +14,21 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Daniel James")
     cy.contains(".appointment__card--show", "Sylvia Palmer")
   })
-  it.only("should edit an interview", () => {
+  it("should edit an interview", () => {
     cy.get("[alt=Edit]").first().click({force:true})
     cy.get("[data-testid=student-name-input]").clear().type("Daniel James")
     cy.get("[alt='Sylvia Palmer']").click()
     cy.contains("Save").click()
     cy.contains(".appointment__card--show", "Daniel James")
     cy.contains(".appointment__card--show", "Sylvia Palmer")
+  })
+  it.only("should edit an interview", () => {
+    cy.get("[alt=Delete]").first().click({force:true})
+    cy.contains("button", "Confirm").click()
+    cy.contains("Deleting")
+    cy.contains("Deleting").should("not.exist")
+    cy.contains(".appointment__card--show", "Archie Cohen")
+    .should("not.exist");
   })
 });
 
